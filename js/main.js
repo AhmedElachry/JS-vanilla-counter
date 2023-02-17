cbb0f2bc89c3d0e560933c7e385c08ab4116c3b7
Ahmed Elashry <91360603+AhmedElachry@users.noreply.github.com>
2023-02-17 18:51:07 +0200

    add some enhancements to easy readability

--- js/main.js
+++ js/main.js
@@ -1,34 +1,31 @@
 // first will catch up our main elements
 
-// displayed counter value
 let counterDisplay = document.querySelector(".counter");
-// input value from user
 let counterValue = document.querySelector(".value");
-// our warning field
 let warningText = document.querySelector(".warning");
-// inc button
 let incBtn = document.querySelector(".increment");
-// dec button
 let decBtn = document.querySelector(".decrement");
-
+let value = 0;
 /////////////////////////////////////////
+isValidNumber(counterValue);
 // input validation
-let value = 0; // (= 0;) this is the simple method to avoid appering NaN to the user was before like this (let value;)
-counterValue.oninput = () => {
-  value = +counterValue.value;
-  return (
-    isNaN(value)
-      ? ((warningText.innerHTML = "please add a valid number"), (value = 0))
-      : value < 0
-      ? (warningText.innerHTML = "please add a positive number")((value = 0))
-      : (warningText.innerHTML = ""),
-    value
-  );
-};
-
+function isValidNumber(counterValue) {
+  counterValue.oninput = () => {
+    value = +counterValue.value;
+    if (isNaN(value)) {
+      warningText.innerHTML = "please add a valid number";
+      value = 0;
+    } else if (value < 0) {
+      warningText.innerHTML = "please add a positive number";
+      value = 0;
+    } else {
+      warningText.innerHTML = "";
+    }
+  };
+}
 //  will go now with our main functions
 
-let sumValue = 0; // (= 0;) this is the solution of our previos problem (the sum was NaN) was (let sumValue;)
+let sumValue = 0;
 
 incBtn.onclick = () => {
   sumValue += value;
